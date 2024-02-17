3c82085cd2ea332883dfc4435ff624dde4cd7c8c
awriyou <febriaryo@gmail.com>
2024-02-18 00:36:36 +0700

    init project part 2

--- App.js
+++ App.js
@@ -10,68 +10,12 @@ import {
   Pressable,
 } from 'react-native';
 
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [modalIsVisible, setModalIsVisible] = useState(false)
-
-  function visibleModal(){
-    setModalIsVisible(true)
-  }
-
-  function visibleCloseModal(){
-    setModalIsVisible(false)
-  }
-
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-    visibleCloseModal()
-  }
-
-  function deleteGoalHandler(id) {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((goal) => goal.id !== id);
-    });
-  }
 
   return (
 
     <View style={styles.appContainer} className="bg-purple-300">
-    <StatusBar style={'light'}/>
-      <Pressable className="px-2 py-4 bg-purple-500 items-center rounded-md" onPress={visibleModal}>
-        <Text className="font-bold text-white">Add Goals</Text>
-      </Pressable>
-      <GoalInput onAddGoal={addGoalHandler} visible={modalIsVisible} onCancel={visibleCloseModal}/>
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalHandler}
-              />
-            );
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
-        />
-        {/* <ScrollView alwaysBounceVertical={false}>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView> */}
-      </View>
     </View>
   );
 }
@@ -83,8 +27,4 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
 
-  goalsContainer: {
-    flex: 5,
-    marginTop: 20,
-  },
 });
